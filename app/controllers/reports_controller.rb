f654403c6b84c9c43876756f9ac58b6bb2ff059c
Thomas_Hansen <ctrhansen@yahoo.com>
2019-03-26 15:05:50 -0500

    Tom In Progress

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,45 @@
+class ReportsController < ApplicationController
+
+#create
+def new
+    @report = Report.new
+end
+
+def create
+    @report = Report.create(report_params)
+end
+
+
+#read
+def show
+    @report = Report.find(params[:id])
+end
+
+def index
+    @reports = Report.all
+end
+
+#update
+def edit
+    @report = Report.find(params[:id])
+end
+
+def update
+    @report = Report.find(params[:id])
+    @report = Report.update(report_params)
+end
+
+#destroy
+def destroy
+    @report = Report.find(params[:id])
+    @report.destroy
+    redirect_to '/reports'
+end
+
+
+#strong_params
+def report_params
+    params.require(:report).permit(:campaign_id, :name, :report_type, :client, :duration, :roi, :impressions, :views, :clicks, :reach, :conversions)
+end
+
+end
