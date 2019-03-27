1b282c4485b8923b49c87ee60f0ac95a496cac4c
Thomas_Hansen <ctrhansen@yahoo.com>
2019-03-27 12:08:29 -0500

    sign in included

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,14 +3,7 @@ class Ad < ApplicationRecord
     has_many :campaigns
     has_many :employees, through: :campaigns
 
-    def list_of_ads_by_employee
-         
-
-    end
-
-    def  
-
-    end
+    
 
 
 
