ecd8cc61c18484e6a11d1a8a5bda0b9e17c0d7c8
github username <github email address>
2019-03-28 18:07:00 -0500

    more olivia

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ApplicationController 
+class UsersController < ApplicationController
 
     def index
 
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-    def create 
+    def create
         @user = User.new(user_params)
         if @user.valid?
             @user.save
-            redirect_to root_path
+            redirect_to users_path
         else
             @user.errors.full_messages
             redirect_to new_user_path(@user)
